8960d81ba631f6a292b46efee962a162d07acd15
raj singh <rajsingh@rajs-MacBook-Air.local>
2024-09-20 13:58:29 +0530

    updated

---
+++ 3. duplicate_emails.sql
@@ -0,0 +1,44 @@
+3. Duplicate emails:https://leetcode.com/problems/duplicate-emails/description/
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write a solution to report all the duplicate emails. Note that its guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+SOL1 
+select 
+   email
+from Person 
+GROUP BY email having count(email) > 1;

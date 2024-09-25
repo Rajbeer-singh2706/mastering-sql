3d5ba9c6957c077281443b7f9b9caf92902377e0
raj singh <rajsingh@rajs-MacBook-Air.local>
2024-09-25 09:29:36 +0530

    updated

--- links.sql
+++ links.sql
@@ -4,14 +4,13 @@ I solved these 20 Leetcode questions.
 
 1. Combine two tables: https://lnkd.in/dumdCXC7
 2. Employees earning > managers: https://Inkd.in/dKqcFmzY
-
 4. Customers who never order : https://lnkd.in/dPZPAKFf
 5. Delete duplicate emails: https://lnkd.in/dEKnD9s3
 6. Rising temperature: https://lnkd.in/dyZvVYvP
 7. Employee bonus: https://Inkd.in/d9iTXt-V
 8. Find customer referee : https://lnkd.in/d5cXjXdb
 10. Big countries: https://lnkd.in/dAnS54qK
-11. Classes more than 5 students : https://lnkd.in/dfrUiutd
+
 12. Sales person: https://lnkd.in/dwRnf5Df
 13. Triangle judgement: https://lnkd.in/dwzD-hFn
 14. Biggest single number: https://lnkd.in/d4F5zHDs
